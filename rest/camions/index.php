bc71591f4e08698f3b8d89aecab96d1b0a7fa8c3
Unknown <ottavio.bnm@eduge.ch>
2018-06-07 16:41:04 +0200

    Correction d'un commentaire
    Correction d'un commentaire dans index.php

--- rest/camions/index.php
+++ rest/camions/index.php
@@ -8,6 +8,7 @@
  * Version      : 1.0
  */
 
+require '../pdo.php';
 require "../produits/produitFoodTruck.php";
 require "../avis/index.php";
 
@@ -30,14 +31,16 @@ class FoodTruck {
 }
 
 /**
- * Récupère tous les food truck de la base de donnée et les transforme en JSON
+ * Récupère tous les food truck de la base de donnée et les transforme en JSON qui sont à une position le jour courant
  * @return array de json - tous les food trucks dans un tableau
  */
-function getFoodTrucks() {
+function getFoodTrucks($latUtilisateur, $lonUtilisateur) {
     $today = date('l');
     $db = getDB();
-    $request = $db->prepare("SELECT TFOODTRUCK.idFoodTruck, Nom, Image, idProprietaire, TLOCALISATION.Latitude, TLOCALISATION.Longitude, DATE_FORMAT(TESTA.HoraireDebut, '%H:%i') AS 'HoraireDebut', DATE_FORMAT(TESTA.HoraireFin, '%H:%i') AS 'HoraireFin', TESTA.JourSemaine, TFOODTRUCK.Contact FROM TFOODTRUCK, TESTA, TLOCALISATION WHERE TESTA.JourSemaine = :jSemaine AND TESTA.idFoodTruck = TFOODTRUCK.idFoodTruck AND TESTA.idLocalisation = TLOCALISATION.idLocalisation");
+    $request = $db->prepare("SELECT TFOODTRUCK.idFoodTruck, Nom, Image, idProprietaire, TLOCALISATION.Latitude, TLOCALISATION.Longitude, DATE_FORMAT(TESTA.HoraireDebut, '%H:%i') AS 'HoraireDebut', DATE_FORMAT(TESTA.HoraireFin, '%H:%i') AS 'HoraireFin', TESTA.JourSemaine, TFOODTRUCK.Contact FROM TFOODTRUCK, TESTA, TLOCALISATION WHERE TESTA.JourSemaine = 'Thursday' AND TESTA.idFoodTruck = TFOODTRUCK.idFoodTruck AND TESTA.idLocalisation = TLOCALISATION.idLocalisation ORDER BY (6378137 * (PI()/2 - ASIN( SIN((PI()*:lat /180)) * SIN((PI()*TLOCALISATION.latitude/180)) + COS((PI()*:lon/180) -(PI()*TLOCALISATION.longitude/180)) * COS((PI()*TLOCALISATION.latitude/180)) *COS((PI()*:lat/180)))))");
     $request->bindParam('jSemaine', $today, PDO::PARAM_STR);
+    $request->bindParam('lat', $latUtilisateur);
+    $request->bindParam('lon', $lonUtilisateur);
     $request->execute();
     $data = $request->fetchAll(PDO::FETCH_ASSOC);
     $array = array();
@@ -59,5 +62,9 @@ function getFoodTrucks() {
     return json_encode($array);
 }
 
+$latitude = filter_input(INPUT_GET, 'lat', FILTER_VALIDATE_FLOAT);
+$longitude = filter_input(INPUT_GET, 'lon', FILTER_VALIDATE_FLOAT);
+error_log($latitude);
+
 // Affichage du Json
-echo getFoodTrucks();
+echo getFoodTrucks($latitude, $longitude);
